83ee9ce2997944d68b9ca53addd72995ff487e80
kostat <kostat@gmail.com>
2015-09-21 11:34:27 +0300

    Revert "add getters to EventHubClient"
    This reverts commit 90f63b0a260436e235961b8806cd6c637c874283.

--- src/main/java/com/microsoft/eventhubs/client/EventHubClient.java
+++ src/main/java/com/microsoft/eventhubs/client/EventHubClient.java
@@ -35,14 +35,6 @@ public class EventHubClient {
     this.entityPath = entityPath;
     connection = createConnection();
   }
-  
-  public String getConnectionString() {
-	return connectionString;
-  }
-  
-  public String getEntityPath() {
-	return entityPath;
-  }
 
   /**
    * creates a new instance of EventHubClient using the supplied connection string and entity path.
